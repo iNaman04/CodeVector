fd7f585a7287520133e60fe181fbec44c885649f
Namandeep Singh <naman25singh@gmail.com>
2026-06-26 13:46:40 +0530

    limitquery

--- src/controllers/productController.js
+++ src/controllers/productController.js
@@ -3,7 +3,10 @@ import getAllProducts from "../services/productService.js";
 
 const getProducts = async(req, res) =>{
     try {
-        const products = await getAllProducts();
+        
+        const limit = parseInt(req.query.limit) || 2;
+        
+        const products = await getAllProducts(limit);
 
         res.status(200).json(products);
     } catch (error) {
--- src/services/productService.js
+++ src/services/productService.js
@@ -1,11 +1,24 @@
 import pool from "../db/db.js";
 
-const getAllProducts = async () => {
-    const result = await pool.query(`
-        SELECT NOW();
-    `);
+export const getAllProducts = async (limit) => {
+
+    const query = `
+        SELECT
+            id,
+            name,
+            category,
+            price,
+            created_at,
+            updated_at
+        FROM products
+        ORDER BY updated_at DESC
+        LIMIT $1;
+    `;
+
+    const result = await pool.query(query, [limit]);
 
     return result.rows;
+
 };
 
 export default getAllProducts;
